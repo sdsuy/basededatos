1d6cf2e14652475ebe731b7b3d996d9bd9594276
sdsuy <sdasilva@outlook.es>
2020-08-14 20:36:42 -0300

    SQL para borrar las tablas

---
+++ Reset.sql
@@ -0,0 +1,5 @@
+/* Elimino las tablas de la base de datos en caso de que ya existan para comenzar de cero */
+DROP TABLE ROL_FUNCION;
+DROP TABLE PERSONA;
+DROP TABLE ROL;
+DROP TABLE FUNCIONALIDAD;
