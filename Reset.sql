bc3559abf6d9ba5971c3db24bbb0ddf2712bffa5
sdsuy <sdasilva@outlook.es>
2020-08-14 20:52:37 -0300

    agregue ademas el creado para hacer todo en un solo paso

--- Reset.sql
+++ Reset.sql
@@ -3,3 +3,10 @@ DROP TABLE ROL_FUNCION;
 DROP TABLE PERSONA;
 DROP TABLE ROL;
 DROP TABLE FUNCIONALIDAD;
+-- Ejecutar hasta aquí si sólo se desea eliminar...
+/* Ejecuto el siguiente escript para el creado de las tablas */
+@@Funcionalidad.sql
+@@Rol.sql
+@@Persona.sql
+@@Rol_Funcion.sql
+
